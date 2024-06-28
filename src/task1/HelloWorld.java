bb656e198357a29cb14aad440bb8a6c7ccc2df39
Stanislav <stanislav.frolov@customertimes.com>
2024-06-28 16:38:53 +0200

    task2

--- src/task1/HelloWorld.java
+++ src/task1/HelloWorld.java
@@ -3,5 +3,13 @@ package task1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int a = 2;
+        int b = a++ + (a * --a);
+        System.out.println(b);
+
+        int c = 4;
+        int d = c >> 1;
+        System.out.println(d);
     }
 }
